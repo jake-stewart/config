969ff5e49483e15db5396ab98e9679fb22618d80
Jake Stewart <jake@systemic.com.au>
2022-05-11 16:06:56 +0800

    update

--- post_install.sh
+++ post_install.sh
@@ -10,3 +10,14 @@ for file in $FILES_NEED_EXECUTABLE; do
         chmod +x "$file"
     fi
 done
+
+mkdir -p $HOME/.config/zsh
+[[ -e $HOME/.config/zsh/zsh-autocomplete ]] || \
+    git clone https://github.com/marlonrichert/zsh-autocomplete $HOME/.config/zsh/zsh-autocomplete
+[[ -e $HOME/.config/zsh/zsh-autosuggestions ]] || \
+    git clone https://github.com/zsh-users/zsh-autosuggestions $HOME/.config/zsh/zsh-autosuggestions
+[[ -e $HOME/.config/zsh/zsh-system-clipboard ]] || \
+    git clone https://github.com/kutsan/zsh-system-clipboard $HOME/.config/zsh/zsh-system-clipboard
+[[ -e $HOME/.config/zsh/zsh-syntax-highlighting ]] || \
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting $HOME/.config/zsh/zsh-syntax-highlighting
+
